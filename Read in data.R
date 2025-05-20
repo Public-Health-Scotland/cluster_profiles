1f6fdc2c7b67bad823b95977ca312e6fea9fed1e
Andrew Saul <andrew.saul@phs.scot>
2025-05-20 11:41:13 +0000

    data directory changed. NHS_Board option mutated

--- Read in data.R
+++ Read in data.R
@@ -2,7 +2,7 @@
 # Created by Andrew Saul August 2024
 
 # path directory to directories containing data files of interest
-data_dir <- "/conf/linkage/output/GP Clusters"
+data_dir <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/GP cluster profiles/R/data/GP Clusters"
 
 # data file names using regex
 data_common_name <- "^AllData.*\\.csv"
@@ -25,7 +25,8 @@ setDF(combined_dt)
 
 #convert to tibble
 profiles <- as_tibble(combined_dt) %>% 
-  select(-LOOKUP)
+  select(-LOOKUP) |> 
+  mutate(NHS_BOARD = case_when(GEOGRAPHY_TYPE == "Scotland" & NHS_BOARD == "" ~ "NHS Scotland", .default = NHS_BOARD))
 
 rm(list = ls()[ls()!="profiles"])
 
